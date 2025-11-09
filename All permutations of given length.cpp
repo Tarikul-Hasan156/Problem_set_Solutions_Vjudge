842841cfde4f528f852b56e92265e564c5358c02
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-11-09 12:28:42 +0600

    Add files via upload

---
+++ All permutations of given length.cpp
@@ -0,0 +1,28 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    ll x;cin>>x;
+    vector<ll>vv;
+    for(int i=1;i<=x;i++)vv.push_back(i);
+    do {
+        for (auto &x:vv)cout<<x;cout<<endl;
+    }while (next_permutation(vv.begin(),vv.end()));
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
