6bdae9e772a69bcbb0672377a0d8a28b106a018a
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-11-10 03:31:27 +0600

    Add files via upload

---
+++ Permutation Value.cpp
@@ -0,0 +1,27 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    ll x;cin>>x;
+    vector<ll>vv;
+    for (int i=2;i<=x;i++)vv.push_back(i);
+    vv.push_back(1);
+    for (auto &x:vv)cout<<x<<sp;cout<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
