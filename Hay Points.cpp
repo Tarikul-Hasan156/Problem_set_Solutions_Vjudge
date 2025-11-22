ae830b1a2098554bd81b6ae880eba77f65ebd5bd
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-11-22 16:06:52 +0600

    Add files via upload

---
+++ Hay Points.cpp
@@ -0,0 +1,59 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp ' '
+#define endl '\n'
+using namespace std;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+
+void solve ()
+{
+    map<string,ll>mp;
+    ll n,m;cin>>n>>m;
+    for (int i=0;i<n;i++){
+        string s;cin>>s;
+        ll val;cin>>val;
+        mp[s]=val;
+    }
+    cin.ignore();
+    while (m--){
+        string word;
+        ll res=0;
+        while (cin>>word){
+            if (word==".")goto level;
+            auto it=mp.find(word);
+            if (it!=mp.end()){
+                res+=mp[word];
+            }
+        }
+        level:
+        cout<<res<<endl;
+    }
+}
+
+int main ()
+{
+    speed;
+    // int tt;
+    // cin >> tt;
+    // while (tt--)
+    // {
+        solve();
+    // }
+    return 0;
+}
