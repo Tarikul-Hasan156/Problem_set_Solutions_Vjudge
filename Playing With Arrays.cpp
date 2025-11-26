57ba9f50e90a20735a5e3c8e51979836728371b8
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-11-26 14:29:40 +0600

    Add files via upload

---
+++ Playing With Arrays.cpp
@@ -0,0 +1,54 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp ' '
+#define endl '\n'
+using namespace std;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+
+void solve ()
+{
+    ll x;cin>>x;
+    vector<ll>R;
+    for (int i=1;i<=x;i++){
+        R.push_back(i);
+    }
+    deque<ll>A;
+    while (!R.empty()){
+        if (!A.empty()){
+            ll back=A.back();
+            A.pop_back();
+            A.push_front(back);
+        }
+        A.push_front(R.back());
+        R.pop_back();
+    }
+    for (auto &x:A)cout<<x<<sp;cout<<endl;
+}
+
+int main ()
+{
+    speed;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
