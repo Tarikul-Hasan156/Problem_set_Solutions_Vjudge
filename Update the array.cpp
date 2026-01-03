c4cca8a2a08318ac70b0e73365c289b1a487afd0
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-01-03 23:49:15 +0600

    Add files via upload

---
+++ Update the array.cpp
@@ -0,0 +1,54 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+// vector<ll>vv(N);
+ll n,x;
+void solve ()
+{
+    cin>>n>>x;
+    vector<ll>vv(n+5);
+    for (int i=1;i<=x;i++){
+        ll l,r,val;cin>>l>>r>>val;
+        l++,r++;
+        vv[l]+=val;
+        vv[r+1]-=val;
+    }
+    for (int i=1;i<=n;i++)vv[i]+=vv[i-1];
+    int q;cin>>q;
+    while (q--){
+        ll index;cin>>index;index++;
+        cout<<vv[index]<<endl;
+    }
+}
+
+int main ()
+{
+    speed;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
