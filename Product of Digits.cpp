30008c5b81e1ea15a9b919e7fe72788682fe6cdd
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-01-11 21:07:30 +0600

    Add files via upload

---
+++ Product of Digits.cpp
@@ -0,0 +1,57 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+
+void solve ()
+{
+    int n;cin>>n;
+    for (int i=0;i<10;i++){
+        if(i==n){
+            if (i==0){
+                cout << 10<<endl;return;
+            }
+            cout<<i<<endl;return;
+        }
+    }
+    multiset<int>mse;
+    for (int i=9;i>=2;i--){
+        while (n%i==0){
+            mse.insert(i);
+            n/=i;
+        }
+    }
+    if (n!=1){
+        cout<<-1<<endl;return;
+    }
+    for (auto val:mse){
+        cout<<val;
+    }
+    cout<<endl;
+}
+
+int main ()
+{
+    speed;
+    solve();
+    return 0;
+}
