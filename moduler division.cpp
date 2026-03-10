3ecb88134cbdb9b6dfb3101e4de9dc8a3059b9c8
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-03-10 19:05:41 +0600

    Add files via upload

---
+++ moduler division.cpp
@@ -0,0 +1,51 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+int power (int n, int a, int mod){
+    n%=mod;
+    int ans=1%mod;
+    while (a){
+        if (a&1){
+            ans=1LL*ans*n%mod;
+        }
+        n=1LL*n*n%mod;
+        a>>=1;
+    }
+    return ans;
+}
+int res (int a,int b, int n){
+    int inverse_b=power(b,n-2,n);
+    return 1LL*a*inverse_b%n;
+}
+void solve ()
+{
+    int a,b,n;cin>>a>>b>>n;
+    cout<<res(a,b,n)<<endl;
+}
+
+int main ()
+{
+    speed;
+    solve();
+    return 0;
+}
