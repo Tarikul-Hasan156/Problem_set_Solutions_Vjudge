1b5a4b181afd9e1ec2e891fd3ef846e66ef87fec
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-03-15 07:53:03 +0600

    Add files via upload

---
+++ Just Add It.cpp
@@ -0,0 +1,56 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+const int mod=10000007;
+int power (int n, int a){
+    int ans=1%mod;
+    while (a){
+        if (a&1){
+            ans=1LL*ans*n%mod;
+        }
+        n=1LL*n*n%mod;
+        a>>=1;
+    }
+    return ans;
+}
+int S (int n,int k){
+    return ((ll)((ll)power(n-1,k)+power(n,k))%mod+power(n-1,k))%mod;
+}
+int P (int n){
+    return ((ll)((ll)power(n-1,n-1)+power(n,n))%mod+power(n-1,n-1))%mod;
+}
+void solve ()
+{
+    int n,k;
+    while (cin>>n>>k){
+        if (n==0 and k==0)return;
+        else cout<<((ll)S(n,k)+P(n))%mod<<endl;
+    }
+}
+
+int main ()
+{
+    speed;
+    solve();
+    return 0;
+}
