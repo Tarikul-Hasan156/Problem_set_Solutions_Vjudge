64ce7a2a5e74a506d617ebfa846dd44d90ac95c6
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-05-04 12:01:56 +0600

    Add files via upload

---
+++ Chef and Tuples.cpp
@@ -0,0 +1,65 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+vector<int>d;
+void divisor (int n){
+    for (int i=1;i*i<=n;i++){
+        if (n%i==0){
+            d.push_back(i);
+            if (i!=(n/i))d.push_back(n/i);
+        }
+    }
+}
+void solve ()
+{
+    int n,a,b,c;cin>>n>>a>>b>>c;
+    vector<int>A,B;
+    divisor(n);
+    // cout<<d.size()<<endl;
+   for (auto val:d){
+        if (val<=a)A.push_back(val);
+        if (val<=b)B.push_back(val);
+   }
+   
+   ll cnt=0;
+   for (int i=0;i<A.size();i++){
+        for (int j=0;j<B.size();j++){
+          ll val=1LL*A[i]*B[j];
+          if (n%val==0 and n/val<=c)cnt++;
+        }
+   }
+   cout<<cnt<<endl;
+   d.clear();
+}
+
+int main ()
+{
+    speed;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
