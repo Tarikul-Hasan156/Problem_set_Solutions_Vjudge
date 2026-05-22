3b6dc628827d337fe7f80059ef2ce11538e8cf1f
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-05-22 23:38:29 +0600

    Add files via upload

---
+++ Integer Divisibility.cpp
@@ -0,0 +1,55 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+int ca = 0;
+// bool check (string s, int mod) {
+//     for (int i = 0; i < s.size(); i++) {
+        
+//     }
+// }
+void solve ()
+{
+    int n, d; cin >> n >> d;
+    int cnt = 0;
+    ll num=0;
+    while (1) {
+        num = num * 10 + d;
+        cnt++;
+        num %= n;
+        if (num % n == 0) {
+            cout << "Case " << ++ca << ": "<< cnt << endl; return;
+        }
+    }
+}
+
+int main ()
+{
+    speed;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
