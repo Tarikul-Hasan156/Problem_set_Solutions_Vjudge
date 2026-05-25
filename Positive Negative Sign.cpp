7f3e50e4782f61c553602170ebfda1eee854f6e2
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-05-25 23:23:13 +0600

    Add files via upload

---
+++ Positive Negative Sign.cpp
@@ -0,0 +1,47 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+int ca = 0;
+ll sum_of_l_to_r (int r) {
+    return (1LL*r*(r+1)/2);
+}
+void solve ()
+{
+    int n, m; cin >> n >> m;
+    ll left = sum_of_l_to_r (m);
+    ll right = sum_of_l_to_r (2*m) - left;
+    right -= left;
+    cout <<"Case " << ++ca <<": " << (n / (2*m)) * right << endl;
+}
+
+int main ()
+{
+    speed;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
